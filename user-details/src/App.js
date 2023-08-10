c90e47c2778c0cf107a141dfa485b4b8a8501813
Saket Sinha <103985771+saketksinha@users.noreply.github.com>
2023-08-10 15:35:08 +0530

    Update App.js

--- user-details/src/App.js
+++ user-details/src/App.js
@@ -1,17 +1,12 @@
 import './App.css';
 import HorizontalLinearStepper from './components/HorizontalLinearStepper';
 
-
-
 function App() {
   return (
-  
+
     <div className="App">
     <HorizontalLinearStepper/>
-  </div>
-
-    
+  </div>    
   );
 }
-
 export default App;
